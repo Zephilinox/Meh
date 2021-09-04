30d955810b61ab2389383a2b86aa97ed7a27b724
Matthew Cheung <m.cheung@virtalis.com>
2021-09-04 15:35:13 +0100

    writing data to packets

--- CoreCommon/src/CoreCommon/WebSocket/WebSocketPackets.hpp
+++ CoreCommon/src/CoreCommon/WebSocket/WebSocketPackets.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cstdint>
+#include <vector>
+
 namespace meh::common::network
 {
 enum class MsgClientServer
@@ -27,14 +30,39 @@ struct PacketHeader
 };
 
 template <typename T>
-struct Packet
+class Packet
 {
+    Packet() = delete;
+    explicit Packet(const PacketHeader& header) : header(header) {}
+    ~Packet() = default;
+    
+    void write(const void* src, size_t size)
+    {
+        if(src)
+        {
+            auto data = static_cast<uint8_t*>(src);
+            body.insert(body.end(), data, data + size);
+        }
+        else
+        {
+            body.insert(body.end(), size, 0);
+        }
+        
+        header.size += size;
+    }
+    
+    template<typename T>
+    void write(const T& src) { write(static_cast<void*>(src)); }
+    template <typename T>
+    void write(std::vector<T>& vec) { write(vec.data(), vec.size() * sizeof(T)) }
+
+    size_t getSize() const { return sizeof(message_header<T>) + body.size(); }
+    
+    const PacketHeader& getHeader() const { return header; }
+    
+    bool empty() const { return body.empty() }
+protected:
     PacketHeader<T> header;
     std::vector<uint8_t> body;
-
-    size_t getSize() const
-    {
-        return sizeof(message_header<T>) + body.size();
-    }
 };
 } // namespace meh::common::network
